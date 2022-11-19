58f6695f2adb5970ab78d6d73394728072550a9f
陶鸿杰 <18790315365@163.com>
2022-11-20 01:28:05 +0800

    feat: 完成基本的测试case

--- src/button/index.test.tsx
+++ src/button/index.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Button from "./index";
 
 test("renders button", () => {
@@ -8,8 +8,55 @@ test("renders button", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test("renders normal button", () => {
+  const { container } = render(<Button type="normal">normal button</Button>);
+  // eslint-disable-next-line
+  expect(container.querySelector(".ant-btn-normal")).toBeInTheDocument();
+});
+
 test("renders primary button", () => {
-  render(<Button type="primary">click me</Button>);
+  const { container } = render(<Button type="primary">primary button</Button>);
+  // eslint-disable-next-line
+  expect(container.querySelector(".ant-btn-primary")).toBeInTheDocument();
+});
+
+test("should support click", () => {
+  const onClick = jest.fn();
+  render(
+    <Button type="primary" onClick={onClick}>
+      support click
+    </Button>
+  );
+  const linkElement = screen.getByText(/support click/i);
+  fireEvent.click(linkElement);
+
+  expect(onClick).toBeCalled();
+});
+
+test("should support blur", () => {
+  const onBlur = jest.fn();
+  render(
+    <Button type="primary" onBlur={onBlur}>
+      click me
+    </Button>
+  );
   const linkElement = screen.getByText(/click me/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(linkElement);
+  fireEvent.blur(linkElement);
+
+  expect(onBlur).toBeCalled();
+});
+
+test("should support focus", () => {
+  const onFocus = jest.fn();
+  render(
+    <Button type="primary" onFocus={onFocus}>
+      click me
+    </Button>
+  );
+  const linkElement = screen.getByText(/click me/i);
+  fireEvent.click(linkElement);
+  fireEvent.blur(linkElement);
+
+  expect(onFocus).toBeCalled();
 });
--- src/button/index.tsx
+++ src/button/index.tsx
@@ -2,22 +2,41 @@ import React, { ReactNode } from "react";
 import classnames from "classnames";
 import "./index.css";
 
-type Props = {
+interface Props extends React.HTMLAttributes<HTMLButtonElement> {
   className?: string;
   type?: "normal" | "primary" | "dashed" | "text" | "link";
+  size?: "small" | "medium" | "large";
   children: ReactNode;
   style?: React.CSSProperties;
-};
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onBlur?: React.FocusEventHandler<HTMLButtonElement>;
+}
 
 const Button = (props: Props) => {
-  const { children, type = "normal", className, style } = props;
+  const {
+    children,
+    type = "normal",
+    className,
+    style,
+    onClick,
+    onBlur,
+    size = "medium",
+    ...others
+  } = props;
   const cls = classnames({
     "ant-btn": true,
     [`ant-btn-${type}`]: type,
+    [`ant-btn-${size}`]: size,
     [className as string]: !!className,
   });
   return (
-    <button className={cls} style={style}>
+    <button
+      className={cls}
+      style={style}
+      onClick={onClick}
+      onBlur={onBlur}
+      {...others}
+    >
       {children}
     </button>
   );
