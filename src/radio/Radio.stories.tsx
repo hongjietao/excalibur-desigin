639024edc92fe0c64d00d1e8c2457a044e063c3e
陶鸿杰 <18790315365@163.com>
2022-11-24 00:42:38 +0800

    feat: set Story Book

--- src/radio/Radio.stories.tsx
+++ src/radio/Radio.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Radio from ".";
+import Radio from "./Radio";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
--- src/tag/Tag.stories.tsx
+++ src/tag/Tag.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Tag from ".";
+import Tag from "./Tag";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -10,13 +10,13 @@ export default {
 } as ComponentMeta<typeof Tag>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-// const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
+const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
 
-// export const Primary = Template.bind({});
-// // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// Primary.args = {
-//   type: "fixed",
-// };
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  children: "Primary",
+};
 
 const log = (e: React.MouseEvent<HTMLElement>) => {
   console.log(e);
